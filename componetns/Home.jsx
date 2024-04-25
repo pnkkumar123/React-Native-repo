91fcd49d851db5c09a03b3838b71030a0dca5297
pnkkumar123 <pankajkanwar420@gmail.com>
2024-04-25 11:53:56 +0530

    added assets folder in project

--- componetns/Home.jsx
+++ componetns/Home.jsx
@@ -1,20 +1,40 @@
-import { View, Text, StatusBar, Platform } from 'react-native'
+import { View, Text, StatusBar, Platform, Image, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import {SafeAreaView} from 'react-native-safe-area-context'
+import { theme } from '../theme/Index'
+import {MagnifyingGlassIcon} from "react-native-heroicons/outline"
 
-
-const ios = Platform.OS === 'ios';
+// const ios = Platform.OS === 'ios';
 
 const Home = () => {
   return (
-    <View >
-      <SafeAreaView >
-        <StatusBar />
-      </SafeAreaView>
-      {/* <View style={styles.icons}>
+   <View className="flex-1 relative">
+    <StatusBar style="light"/>
+    <Image blurRadius={70} source={require('../assets/images/bg.png')}
+    className="absolute h-full w-full"/>
+    <SafeAreaView className="flex flex-1">
+      <View style={{height:'7%'}} className="mx-4 relative z-50">
+        <View style={{backgroundColor:theme.bgWhite(0.2)}} className="flex-row justify-end items-center rounded-full">
+          <TextInput
+          placeholder='Search city'
+          placeholderTextColor={'lightgray'}
+          className="pl-6 h-10 flex-1 text-base text-white"
+          />
+          <TouchableOpacity
+          style={{backgroundColor:theme.bgWhite(0.3)}}
+          className="rounded-full p-3 m-1"
+          >
 
-      </View> */}
-    </View>
+          <MagnifyingGlassIcon size={25} color="white"/>
+          </TouchableOpacity>
+          
+
+        </View>
+
+      </View>
+
+    </SafeAreaView>
+   </View>
   )
 }
 
